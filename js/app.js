f5b29d3eecfbf091ec2cd23cf6a86252b5881a88
Babsmay47 <babsmay47@gmail.com>
2025-08-23 06:37:47 +0100

    style: update task layout and filtering logic; enhance task rendering and storage management

--- js/app.js
+++ js/app.js
@@ -42,6 +42,8 @@ filterItems.forEach( filterItem => {
   });
 });
 
+
+
 // const task = {
 //   id: uniqueIdentifier,
 //   text: description,
@@ -52,58 +54,39 @@ filterItems.forEach( filterItem => {
 //   createdAt: time
 // }
 
-let tasks = [];
+const tasks = JSON.parse(localStorage.getItem('ToDoList')) || [];
 
-
-const addTodo = function() {
-  const inputElement = document.querySelector('[data-task-input-field]');
-  const catgorySelectElement = document.querySelector('[data-category]');
-  const prioritySelectElement = document.querySelector('[data-priority]');
-  const dueDateElement = document.querySelector('[data-due-date-input]');
-  const taskName = inputElement.value;
-  const taskCategory = catgorySelectElement.value;
-  const taskPriority = prioritySelectElement.value;
-  const dueDate = dueDateElement.value;
-
-  const uniqueId = module.generateUniqueId();
-
-  if(taskName === ''){
-    alert('please fill out...');
-  }
-
-  const task = {
-    id: uniqueId,
-    text: taskName,
-    completed: false,
-    priority: taskPriority,
-    dueDate: dueDate,
-    category: taskCategory,
-    createdAt: Date.now()
-  }
-
-  tasks.unshift(task);
-
-  inputElement.value = '';
-  renderTodo();
-  console.log(dueDate);
-  console.log(tasks);
-}
+renderTodo();
 
 function renderTodo() {
-  const activeTasksContainer = document.querySelector('[data-active-tasks] .task-list');
+  const tasksContainer = document.querySelector('[data-active-tasks]');
 
-  activeTasksContainer.innerHTML = '';
+  tasksContainer.innerHTML = '';
+
+  if(tasks.length === 0){
+    tasksContainer.innerHTML = `
+      <div class="empty-state">
+        <div class="empty-icon">
+          <i class="fa-solid fa-clipboard-list"></i>
+        </div>
+        <h4 class="title-4 empty-title">No Active Tasks</h4>
+        <span>Add your first task to get started</span>
+      </div>
+    `
+    return;
+  }
 
   tasks.forEach(task => {
     const taskElement = document.createElement('div');
 
-    taskElement.classList.add('task-card')
+    taskElement.classList.add('task-card');
 
+    
     taskElement.innerHTML = `
       <div class="task-header">
-        <input type="checkbox" name="task-checkbox" id="task-checkbox" class="task-checkbox" data-completed-checkbox>
+        <input type="checkbox" name="task-checkbox" id="task-checkbox" class="task-checkbox" onclick="toggleTaskComplete(${task.id});" data-completed-checkbox>
         <h4 class="task-title">
-          ${task.text}
+          ${task.title}
         </h4>
       </div>
 
@@ -122,23 +105,77 @@ function renderTodo() {
           </button>
         </div>
       </div>
-    `
+    ` 
     
     const completedBtn = document.querySelector('[data-completed-checkbox]');
     const deleteBtn = document.querySelector('[data-task-delete-btn]');
     const editBtn = document.querySelector('[data-task-edit-btn]');
 
-    completedBtn.addEventListener('click', toggleTaskCompleted(task.id));
-    deleteBtn.addEventListener('click', toggleTaskCompleted(task.id));
-    editBtn.addEventListener('click', toggleTaskCompleted(task.id));
+    // completedBtn.addEventListener('click', toggleTaskComplete(task.id));
+    // deleteBtn.addEventListener('click', toggleTaskComplete(task.id));
+    // editBtn.addEventListener('click', toggleTaskComplete(task.id));
 
-    activeTasksContainer.appendChild(taskElement);
+    tasksContainer.appendChild(taskElement);
+    // renderTodo();
+    saveToStorage();
   });
 
 }
+
+function addTodo() {
+  const inputElement = document.querySelector('[data-task-input-field]');
+  const catgorySelectElement = document.querySelector('[data-category]');
+  const prioritySelectElement = document.querySelector('[data-priority]');
+  const dueDateElement = document.querySelector('[data-due-date-input]');
+  const taskTitle = inputElement.value;
+  const taskCategory = catgorySelectElement.value;
+  const taskPriority = prioritySelectElement.value;
+  const dueDate = dueDateElement.value;
+  const uniqueId = module.generateUniqueId();
+
+  if (!taskTitle.trim()) return;
+
+  const task = {
+    id: uniqueId,
+    title: taskTitle,
+    completed: false,
+    priority: taskPriority,
+    dueDate: dueDate,
+    category: taskCategory,
+    createdAt: Date.now()
+  }
+
+  tasks.unshift(task);
+
+  inputElement.value = '';
+  renderTodo();
+  saveToStorage();
+  console.log(dueDate);
+  console.log(tasks);
+}
+
+function toggleTaskComplete(id) {
+  const task = tasks.find(t => t.id === id);
+
+  if(task) {
+    task.completed = !task.completed;
+
+    // renderTodo();
+    saveToStorage();
+  }
+}
+
+
+
 
 document.querySelector('.add-task-btn').addEventListener('click', (e) => {
   addTodo();
   e.preventDefault();
   console.log('Reload prevented!');
 });
+
+function saveToStorage() {
+  localStorage.setItem('ToDoList', JSON.stringify(tasks));
+}
+
+console.log(tasks);
